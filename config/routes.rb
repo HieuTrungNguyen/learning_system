f7b859c4ad020a8bea6202ed988e4b64e40e2820
Hieu Trung Nguyen <nguyentrunghieu3103@gmail.com>
2018-03-18 17:05:06 +0700

    user list category

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   delete "logout", to: "sessions#destroy"
 
   resources :users
+  resources :categories, only: [:index, :show]
 
   namespace :admin do
     resources :categories
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,19 @@ User.create!(name: "Hieu Trung Nguyen",
     password: password,
     password_confirmation: password)
 end
+
+# Category
+50.times do |n|
+  name = Faker::Lorem.sentence(2, true, 0)
+  description = Faker::Lorem.sentence
+  Category.create!(
+    name: name,
+    description: description)
+end
+
+# users = User.all
+# user  = users.first
+# following = users[2..50]
+# followers = users[3..40]
+# following.each { |followed| user.follow(followed) }
+# followers.each { |follower| follower.follow(user) }
