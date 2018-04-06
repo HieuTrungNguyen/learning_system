9abf45416f64b210e5b09cc693fbcce5e8d47519
Hieu Trung Nguyen <nguyentrunghieu3103@gmail.com>
2018-04-06 08:08:31 +0700

    fix bug up heroku

--- db/migrate/20180406010618_create_activities.rb
+++ db/migrate/20180406010618_create_activities.rb
@@ -1,9 +1,9 @@
 class CreateActivities < ActiveRecord::Migration[5.1]
   def change
     create_table :activities do |t|
-      t.references :user, index: true, foreign_key: true
       t.integer :action_type
       t.integer :target_id
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps
     end
