236ceeb12fc483b9a20ec5ec2030bde715001cad
Hieu Trung Nguyen <nguyentrunghieu3103@gmail.com>
2018-05-03 21:43:25 +0700

    create activity

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,6 +4,8 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find_by id: params[:followed_id]
     current_user.follow @user
+    @user.activities.create user_id: current_user.id,
+      activity_type: Activity.actions[:follow]
     respond_to do |format|
       format.html {redirect_to @user}
     end
@@ -12,6 +14,8 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = Relationship.find_by(id: params[:id]).followed
     current_user.unfollow @user
+    @user.activities.create user_id: current_user.id,
+      activity_type: Activity.actions[:unfollow]
     respond_to do |format|
       format.html {redirect_to @user}
     end
